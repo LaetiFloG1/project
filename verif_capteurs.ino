51e379998e803b79d76b6f1be3b08fc7dddeec22
Laetitia&Florian <laetig83@gmail.com>
2020-01-06 21:33:38 +0100

    PROG_CAPTEURS

---
+++ verif_capteurs.ino
@@ -0,0 +1,15 @@
+const int capt=2;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(capt, INPUT);
+
+}
+
+void loop() {
+if (capt=1){
+    Serial.println("ok");
+  }
+  // put your main code here, to run repeatedly:
+
+}
